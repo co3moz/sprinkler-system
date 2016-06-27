36404f3a42e327c126e1149c7059a4c9b15c18ed
Doğan Derya <doganderya59@gmail.com>
2016-06-27 12:44:47 +0300

    pin fix

--- src/jobs/pinController.js
+++ src/jobs/pinController.js
@@ -30,17 +30,26 @@ module.exports = () => {
     }
   }).then((taps) => {
     taps.forEach((tap) => {
-      if (!pins[tap.gpio]) pins[tap.gpio] = new gpio(tap.gpio, 'out');
+      if (!pins[tap.gpio]) {
+        logger.debug('pin {0} opened', tap.gpio);
+        pins[tap.gpio] = new gpio(tap.gpio, 'out');
+      }
 
       if (tap.status == 'OPERATIVE') {
-        pins[tap.gpio].write(1);
+        pins[tap.gpio].writeSync(1);
         logger.debug('pin {0} setted to {1}', tap.gpio, 1);
       } else {
-        pins[tap.gpio].write(0);
+        pins[tap.gpio].writeSync(0);
         logger.debug('pin {0} setted to {1}', tap.gpio, 0);
-        pins[tap.gpio].unexport();
-        delete pins[tap.gpio];
       }
     });
   });
 };
+
+process.on('exit', (code) => {
+  Object.keys(pins).forEach((pin) => {
+    logger.debug('pin {0} closed', pin);
+    pins[pin].writeSync(0);
+    pins[pin].unexport();
+  })
+});
