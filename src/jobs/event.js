e2bca75350aff86f8de84416d9b6ac156c3290fb
Doğan Derya <doganderya59@gmail.com>
2016-06-28 16:00:42 +0300

    1.1.2

--- src/jobs/event.js
+++ src/jobs/event.js
@@ -2,6 +2,6 @@ const schedule = require('node-schedule');
 const config = require('config');
 
 schedule.scheduleJob(config.get('jobs.mainTimer'), () => {
-  require('./mainTimer');
-  require('./pinController');
+  require('./mainTimer')();
+  require('./pinController')();
 });
