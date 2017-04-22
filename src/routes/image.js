03a0712b38260fef29a888a96f97657f0ed40c04
Doğan Derya <doganderya59@gmail.com>
2017-04-22 21:45:46 +0300

    stuff

--- src/routes/image.js
+++ src/routes/image.js
@@ -5,7 +5,7 @@ const router = gluon.router();
 router.get('/', (req, res) => {
   exec('fswebcam -d /dev/video0 -S 20 -s brightness=150% -s Contrast=150% -s Gamma=150% -r 640x480 -s Sharpness =50% -s Saturation=50% --fps 5 --save test2.png --png 10', (err, stdout, stderr) => {
     if (err) {
-      res.ok(err);
+      res.ok("{0:j:4}\n  stdout:{1}\n  stderr: {2}".format(err, stdout, stderr));
       return;
     }
 
